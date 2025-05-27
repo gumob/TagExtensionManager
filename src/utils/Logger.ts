b69dc4b23fc7c6d13a11ce784c4c159c67a9faca
Kojiro Futamura <gumob.dev@gmail.com>
2025-05-27 23:17:16 +0900

    Update logger configuration for improved logging format - Changed logger name from 'ExtensionManager' to 'CEM' for better identification. - Added a pretty log template for enhanced log readability.

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -4,7 +4,9 @@ import { Logger as TSLogger } from 'tslog';
  * The logger instance.
  */
 export const logger = new TSLogger({
-  name: 'ExtensionManager',
+  name: 'CEM',
+  prettyLogTemplate:
+    '{{hh}}:{{MM}}:{{ss}}:{{ms}} {{logLevelName}} [{{name}}][{{fileNameWithLine}}] ',
   type: 'pretty',
   minLevel: process.env.NODE_ENV === 'development' ? 0 : 2, // 0: debug, 2: info
 });
